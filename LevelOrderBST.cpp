2f00ff13b6dd96d0a21a445b0ebefa20ac431efc
Snehdeep Singh <snehdeepsingh221199@gmail.com>
2021-10-31 14:36:14 +0530

    Prob 04

---
+++ LevelOrderBST.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *createNewNode(int value) {
+    Node *temp = new Node;
+    temp->data = value;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+Node *newNode(Node *root, int data) {
+    if (root == NULL)
+        root = createNewNode(data);
+    else if (data < root->data)
+        root->left = newNode(root->left, data);
+    else
+        root->right = newNode(root->right, data);
+    return root;
+}
+
+
+vector<int> levelOrder(struct Node* node) {
+
+    vector<int> v;
+    if(node== NULL)
+    return v;
+    queue<Node*> q;
+    q.push(node);
+    while(!q.empty()){
+        Node* curr=q.front();
+        q.pop();
+        v.push_back(curr->data);
+        if(curr->left) q.push(curr->left);
+        if(curr->right) q.push(curr->right);
+    }
+    return v;
+}
+
+
+int main() {
+
+    int testcases;
+    cin >> testcases;
+    while (testcases--) {
+        Node *root = NULL;
+        int sizeOfArray;
+        cin >> sizeOfArray;
+        int arr[sizeOfArray];
+
+        for (int i = 0; i < sizeOfArray; i++) cin >> arr[i];
+
+        for (int i = 0; i < sizeOfArray; i++) {
+            root = newNode(root, arr[i]);
+        }
+
+        vector<int> res = levelOrder(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
